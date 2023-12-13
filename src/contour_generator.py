8d70efd0ac8f8507a3c2f1339e2b859238ae20a7
Martin Palkovic <martin@palkovic.org>
2023-12-13 16:17:48 -0400

    Updates

--- src/contour_generator.py
+++ src/contour_generator.py
@@ -1,39 +1,12 @@
-import rasterio
-from rasterio import features
-import geopandas as gpd
-import matplotlib.pyplot as plt
+from qgis.core import *
 
-def generate_contour_lines(input_raster_path, output_plot=True):
-    """
-    Generate contour lines from a raster file and optionally plot them.
+raster = [l for l in QgsProject().instance().mapLayers().values() if isinstance(l, QgsRasterLayer) and 'LochAlvaLidar' in l.name()][0]
+result = processing.run("gdal:contour",
+    {'INPUT':raster,
+    'BAND': 1,
+    'INTERVAL':10,
+    'FIELD_NAME':'ELEV',
+    'OUTPUT':'TEMP_OUTPUT'})
 
-    Parameters:
-    - input_raster_path (str): Path to the input raster file.
-    - output_plot (bool): Whether to plot the generated contour lines (default is True).
-
-    Returns:
-    - gdf (GeoDataFrame): GeoDataFrame containing contour lines.
-    """
-
-    # Open the raster file
-    with rasterio.open(input_raster_path) as src:
-        # Read the raster data
-        raster_data = src.read(1)
-
-        # Generate contours
-        contours = features.shapes(raster_data, transform=src.transform)
-
-        # Convert contours to GeoDataFrame
-        gdf = gpd.GeoDataFrame(geometry=[shape for shape, value in contours],
-                               data={'value': [value for shape, value in contours]})
-
-    # Plot the contours if specified
-    if output_plot:
-        gdf.plot(column='value', cmap='viridis', legend=True)
-        plt.show()
-
-    return gdf
-
-# Example usage:
-input_raster_path = input('Please enter the path for your raster file:')
-contour_lines_gdf = generate_contour_lines(input_raster_path, False)
+layer = QgsRasterLayer(result['OUTPUT'])
+QgsProject.instance().addMapLayer(layer)
