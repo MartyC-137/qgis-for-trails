cd7cbf11ff690385547b63f83f340c90c999edb5
Martin Palkovic <martin@palkovic.org>
2023-12-30 15:32:20 -0400

    Updates

--- examples/trail_difficulty_colors.sql
+++ examples/trail_difficulty_colors.sql
@@ -1,6 +1,6 @@
 CASE
 	WHEN "DIFFICULTY" = 'GREEN' then color_rgb(43, 176, 28)
-	WHEN "DIFFICULTY" = 'BLUE' then color_rgb(52, 125, 235)
+	WHEN "DIFFICULTY" = 'BLUE' then color_rgb(17, 93, 191)
 	WHEN "DIFFICULTY" = 'BLACK' then color_rgb(0, 0, 0)
 	WHEN "DIFFICULTY" = 'DOUBLE BLACK' then color_rgb(230, 25, 25)
 END
